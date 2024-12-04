0aa88f5e07ffaee76eccf0a3a181742dc3568c79
KY00KIM <dev.kyoomin@gmail.com>
2024-12-04 15:45:25 +0900

    Add P2PTransport Server

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,6 +11,21 @@ struct Opts {
 async fn main() {
     let opts: Opts = Opts::parse();
     if opts.test_p2p {
-        p2p::p2p_test_run().await;
+        let _ = p2p::P2PTransport::run(4001, false).await;
+    } else {
+        tracing::info!("Server started.");
+        // let _ = p2p::P2PTransport::run(4001, false).await;
+        let (exit_tx, exit_rx) = tokio::sync::oneshot::channel();
+        tokio::task::spawn(async {
+            let _ = p2p::P2PTransport::run_with_restart(4001, 10, exit_rx).await;
+        });
+        println!("Server started. Press Enter to exit.");
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let _ = exit_tx.send(());
+
+        tracing::info!("Server exited.");
     }
 }
