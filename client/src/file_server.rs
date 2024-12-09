4c2193d104cf0d78030664fe0bee89e94817b395
jhjoon09 <jhjoon09@gmail.com>
2024-12-09 11:45:09 +0900

    feat/separate folder and files

--- client/src/file_server.rs
+++ client/src/file_server.rs
@@ -2,42 +2,54 @@ extern crate walkdir;
 
 use crate::config_loader::get_config;
 use kudrive_common::event::client::ClientEvent;
-use kudrive_common::fs::{File, FileMap};
+use kudrive_common::fs::{File, Folder, FileMap};
 use notify::{Event, RecommendedWatcher, RecursiveMode, Result, Watcher};
 use regex::Regex;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc;
 use tokio::sync::mpsc::Sender;
-use walkdir::WalkDir;
+use dirs;
 
 pub struct FileServer {
     responder: Sender<ClientEvent>,
 }
 
-fn list_all_files<P: AsRef<Path>>(path: P) -> FileMap {
+fn get_filemap(path : String) -> FileMap {
+    let resolved_path = if path.starts_with("~") {
+        let home_dir = dirs::home_dir().expect("Failed to get home directory");
+        let home_dir = home_dir.to_str().unwrap().replace("\\", "/");
+        path.replacen("~", &home_dir, 1)
+    } else {
+        path
+    };
+
     let mut files = Vec::new();
+    let mut folders = Vec::new();
 
-    let walker = WalkDir::new(path);
+    let paths = match std::fs::read_dir(&resolved_path) {
+        Ok(entries) => entries,
+        Err(_) => return FileMap { files, folders },
+    };
 
-    let iterator = walker.into_iter();
+    for entry in paths {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            let file_name = match path.file_name() {
+                Some(name) => name.to_str().unwrap().to_string(),
+                None => continue,
+            };
 
-    let filtered_iterator = iterator.filter_map(|e| e.ok());
+            let file_name = format!("{}/{}", resolved_path, file_name);
 
-    for entry in filtered_iterator {
-        if entry.file_type().is_file() {
-            let file_path = entry.path().display().to_string();
-            files.push(file_path);
+            if path.is_dir() {
+                folders.push(Folder { name : file_name });
+            } else {
+                files.push(File { name : file_name });
+            }
         }
     }
 
-    let files = files
-        .iter()
-        .map(|file| File {
-            name: file.to_string(),
-        })
-        .collect();
-
-    return FileMap { files };
+    FileMap { files, folders }
 }
 
 fn check_exclude(path: &PathBuf, patterns: &[Regex]) -> bool {
@@ -59,8 +71,8 @@ impl FileServer {
     }
 
     async fn send(responder: Sender<ClientEvent>) {
-        let files = list_all_files(get_config().file.workspace.clone());
-        let event = ClientEvent::FileMapUpdate { file_map: files };
+        let file_map = get_filemap(get_config().file.workspace.clone());
+        let event = ClientEvent::FileMapUpdate { file_map: file_map.clone() };
 
         // TODO: logics for file map update
         responder.send(event).await.unwrap();
