263a1a328148c215168a5faefec1c0ad2a1a8c66
jhjoon09 <jhjoon09@gmail.com>
2024-12-09 14:10:45 +0900

    feat: make recursive

--- client/src/file_server.rs
+++ client/src/file_server.rs
@@ -14,7 +14,7 @@ pub struct FileServer {
     responder: Sender<ClientEvent>,
 }
 
-fn get_filemap(path : String) -> FileMap {
+fn get_files(path : String) -> FileMap {
     let resolved_path = if path.starts_with("~") {
         let home_dir = dirs::home_dir().expect("Failed to get home directory");
         let home_dir = home_dir.to_str().unwrap().replace("\\", "/");
@@ -52,6 +52,31 @@ fn get_filemap(path : String) -> FileMap {
     FileMap { files, folders }
 }
 
+fn get_filemap(path: String) -> FileMap {
+    let mut all_files = Vec::new();
+    let mut all_folders = Vec::new();
+
+    // 현재 경로의 파일 및 폴더 가져오기
+    let current = get_files(path.clone());
+
+    // 현재 폴더와 파일 추가
+    all_files.extend(current.files);
+    all_folders.extend(current.folders.clone());
+
+    // 하위 폴더를 재귀적으로 탐색
+    for folder in current.folders {
+        let sub_contents = get_filemap(folder.name);
+        all_files.extend(sub_contents.files);
+        all_folders.extend(sub_contents.folders);
+    }
+
+    FileMap {
+        files: all_files,
+        folders: all_folders,
+    }
+}
+
+
 fn check_exclude(path: &PathBuf, patterns: &[Regex]) -> bool {
     path.components().any(|component| {
         let component_str = component.as_os_str().to_string_lossy();
@@ -72,6 +97,7 @@ impl FileServer {
 
     async fn send(responder: Sender<ClientEvent>) {
         let file_map = get_filemap(get_config().file.workspace.clone());
+        println!("{:?}", file_map);
         let event = ClientEvent::FileMapUpdate { file_map: file_map.clone() };
 
         // TODO: logics for file map update
@@ -85,7 +111,6 @@ impl FileServer {
 
         tokio::spawn(async move {
             println!("File server started.");
-
             let config = get_config();
             let path = &config.file.workspace;
             let exclude_patterns = config
@@ -95,7 +120,6 @@ impl FileServer {
                 .map(|pattern| Regex::new(pattern).unwrap())
                 .collect::<Vec<Regex>>();
             let delay_time = config.file.refresh_time as u128;
-
             let (tx, rx) = mpsc::channel::<Result<Event>>();
             let mut watcher: RecommendedWatcher =
                 notify::recommended_watcher(tx).expect("watcher error");
